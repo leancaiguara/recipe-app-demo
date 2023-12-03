3f9d4605245b06c066cfa340e38ca232ec72fdd0
leandro <leancaigura@gmail.com>
2023-12-03 16:59:59 -0300

    check lint

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .settings import INSTALLED_APPS
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+elem12_elemelem = ''
